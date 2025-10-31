a0fb233c361483a8c4840674dcde4d7d6e29bfc9
mrwhy224 <mail@mroohi.ir>
2025-11-01 01:06:02 +0330

    update users model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,40 +9,62 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+	use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = [
+		'first_name',
+		'last_name',
+		'email',
+		'phone',
+		'password',
+		'country_id',
+		'currency_id',
+		'language_id',
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array<string, string>
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+		'phone_verified_at' => 'datetime', // افزودن
+	];
+
+	/**
+	 * (اختیاری اما پیشنهادی)
+	 * یک Accessor برای 'name' ایجاد کنید تا کدهای دیگر به مشکل نخورند.
+	 */
+	public function getNameAttribute()
+	{
+		return $this->first_name . ' ' . $this->last_name;
+	}
+
+	// --- افزودن روابط (Relations) ---
+
+	public function country()
+	{
+		return $this->belongsTo(Country::class);
+	}
+
+	public function language()
+	{
+		return $this->belongsTo(Language::class);
+	}
 }
