c3de2ca9ac55524314b9b2044b54b63d82cf752e
mrwhy224 <mail@mroohi.ir>
2025-11-01 02:39:14 +0330

    add dashboard for user

---
+++ resources/views/vuexy/content/user/dashboard.blade.php
@@ -0,0 +1,37 @@
+@php
+$configData = Helper::appClasses();
+@endphp
+
+@extends('layouts/layoutMaster')
+
+@section('title', 'Dashboard')
+
+@section('vendor-style')
+@vite([
+'resources/assets/vendor/libs/apex-charts/apex-charts.scss',
+'resources/assets/vendor/libs/swiper/swiper.scss',
+'resources/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.scss',
+'resources/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.scss',
+'resources/assets/vendor/fonts/flag-icons.scss'
+])
+@endsection
+@section('page-style')
+@vite('resources/assets/vendor/scss/pages/cards-advance.scss')
+@endsection
+@section('vendor-script')
+@vite([
+'resources/assets/vendor/libs/apex-charts/apexcharts.js',
+'resources/assets/vendor/libs/swiper/swiper.js',
+'resources/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js'
+])
+@endsection
+
+@section('page-script')
+@vite('resources/assets/js/dashboards-analytics.js')
+@endsection
+
+@section('content')
+<div class="row g-6">
+  Dashboard
+</div>
+@endsection
