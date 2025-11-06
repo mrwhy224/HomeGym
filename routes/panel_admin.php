c12266ecbe54fcc73c470cd5dfd23e34ee259c19
mrwhy224 <mail@mroohi.ir>
2025-11-07 00:58:37 +0330

    Add post part into admin menu

--- routes/panel_admin.php
+++ routes/panel_admin.php
@@ -1,6 +1,26 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\user\Dashboard;
+use App\Http\Controllers\post\Category;
+use App\Http\Controllers\post\Comment;
+use App\Http\Controllers\post\PostAdd;
+use App\Http\Controllers\post\PostList;
 
+
+
+Route::group(['prefix' => 'post','as' => 'post.'], function () {
+	Route::get('/', [PostList::class, 'index'])->name('list');
+	Route::get('/create', [PostAdd::class, 'index'])->name('create');
+	Route::get('/comment', [Comment::class, 'index'])->name('comment');
+	Route::get('/category', [Category::class, 'view'])->name('category');
+
+});
+
+Route::group(['prefix' => 'api','as' => 'api.'], function () {
+
+	Route::group(['prefix' => 'category','as' => 'api.'], function () {
+		Route::get('list', [Category::class, 'index'])->name('category.all');
+	});
+});
 
 Route::get('/', [Dashboard::class, 'index'])->name('dashboard-analytics');
