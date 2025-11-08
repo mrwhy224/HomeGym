5b43c5d878874a43a834ee6d2807cb0e001a66a2
mrwhy224 <mail@mroohi.ir>
2025-11-08 22:46:38 +0330

    setup coach dashboard

---
+++ routes/panel_coach.php
@@ -0,0 +1,9 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\coach\Dashboard;
+
+
+
+Route::get('/', [Dashboard::class, 'index'])->name('dashboard');
+
+Route::fallback(function () {return view('content.pages.pages-misc-error');});
