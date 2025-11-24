22fc34ee6882d643a4c8d5674e4c97ecdcbe8892
mrwhy224 <mail@mroohi.ir>
2025-11-25 02:24:44 +0330

    lang popup added

---
+++ resources/lang/en/layout.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'cookie' => [
+        'title' => 'Welcome to Sepfit',
+        'message' => 'By selecting a language and continuing to browse, you agree to our use of cookies and',
+        'policy_link' => 'Privacy Policy',
+    ],
+];
