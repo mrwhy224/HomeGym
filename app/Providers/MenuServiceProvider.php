08197b7d8d918099d0f51b7798198274c5c44c25
mrwhy224 <mail@mroohi.ir>
2025-11-28 03:56:22 +0330

    Update MenuServiceProvider.php

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class MenuServiceProvider extends ServiceProvider
@@ -21,13 +23,23 @@ class MenuServiceProvider extends ServiceProvider
 	 */
 	public function boot(): void
 	{
-		$verticalMenuJson = file_get_contents(base_path('resources/menu/verticalMenu.json'));
-		if (Request::is('admin*'))
-			$verticalMenuJson = file_get_contents(base_path('resources/menu/adminVerticalMenu.json'));
-		elseif (Request::is('coach*'))
-			$verticalMenuJson = file_get_contents(base_path('resources/menu/coachVerticalMenu.json'));
-		elseif (Request::is('user*'))
-			$verticalMenuJson = file_get_contents(base_path('resources/menu/userVerticalMenu.json'));
-		$this->app->make('view')->share('menuData', [json_decode($verticalMenuJson)]);
+		View::composer('*', function ($view) {
+        $jsonPath = base_path('resources/menu/verticalMenu.json');
+
+        if (Auth::check()) {
+            $user = Auth::user();
+
+            if ($user->hasRole('super_admin') || $user->hasRole('content_manager')) {
+                $jsonPath = base_path('resources/menu/adminVerticalMenu.json');
+            } elseif ($user->hasRole('coach')) {
+                $jsonPath = base_path('resources/menu/coachVerticalMenu.json');
+            } elseif ($user->hasRole('customer')) {
+                $jsonPath = base_path('resources/menu/userVerticalMenu.json');
+            }
+        }
+
+        $menuData = [json_decode(file_get_contents($jsonPath))];
+        $view->with('menuData', $menuData);
+    });
 	}
 }
